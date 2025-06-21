c74d3d6b8d69c2420929d75ec96f427b54c218cb
Bilal Orhanlar <bilal.orhanlar149@gmail.com>
2025-06-22 00:25:31 +0300

    v2.4

--- front/src/middleware.js
+++ front/src/middleware.js
@@ -3,11 +3,12 @@ import { NextResponse } from 'next/server'
 export function middleware(request) {
   const response = NextResponse.next()
   
-  // CORS Headers
-  response.headers.set('Access-Control-Allow-Origin', 'https://cekgetir.com', 'https://*.openrouteservice.org', 'https://localhost:3000', 'http://localhost:3000')
+  // CORS Headers - cekgetir.com için düzenlendi
+  response.headers.set('Access-Control-Allow-Origin', 'https://cekgetir.com, https://www.cekgetir.com, https://api.cekgetir.com, https://admin.cekgetir.com, https://localhost:3000, http://localhost:3000')
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH')
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept')
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin, X-API-Key')
   response.headers.set('Access-Control-Allow-Credentials', 'true')
+  response.headers.set('Access-Control-Max-Age', '86400')
   
   // Security Headers
   response.headers.set('X-Frame-Options', 'DENY')
@@ -17,12 +18,12 @@ export function middleware(request) {
 
   const cspDirectives = [
     "default-src 'self'",
-    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com/recaptcha/ https://www.gstatic.com/ https://*.googleapis.com https://maps.googleapis.com https://maps.gstatic.com https://api.openrouteservice.org https://www.googletagmanager.com",
-    "style-src 'self' 'unsafe-inline' https://www.google.com/recaptcha/ https://*.googleapis.com https://fonts.googleapis.com https://cdnjs.cloudflare.com https://api.openrouteservice.org",
-    "img-src 'self' data: blob: https://www.google.com/recaptcha/ https://*.googleapis.com https://*.gstatic.com https://maps.gstatic.com https://maps.googleapis.com https://*.tile.openstreetmap.org https://cdnjs.cloudflare.com https://api.openrouteservice.org",
-    "font-src 'self' https://*.gstatic.com https://fonts.googleapis.com https://cdnjs.cloudflare.com",
-    "connect-src 'self' https://cekgetir.com https://api.cekgetir.com https://cekgetir.up.railway.app https://*.googleapis.com https://maps.googleapis.com https://maps.gstatic.com https://nominatim.openstreetmap.org https://api.openrouteservice.org https://api.opencagedata.com https://*.opencagedata.com https://photon.komoot.io",
-    "frame-src 'self' https://*.googleapis.com https://www.google.com/maps/ https://maps.google.com/ https://www.google.com/ https://*.gstatic.com ",
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com/recaptcha/ https://www.gstatic.com/ https://*.googleapis.com https://maps.googleapis.com https://maps.gstatic.com https://api.openrouteservice.org https://www.googletagmanager.com https://cekgetir.com https://*.cekgetir.com",
+    "style-src 'self' 'unsafe-inline' https://www.google.com/recaptcha/ https://*.googleapis.com https://fonts.googleapis.com https://cdnjs.cloudflare.com https://api.openrouteservice.org https://cekgetir.com https://*.cekgetir.com",
+    "img-src 'self' data: blob: https://www.google.com/recaptcha/ https://*.googleapis.com https://*.gstatic.com https://maps.gstatic.com https://maps.googleapis.com https://*.tile.openstreetmap.org https://cdnjs.cloudflare.com https://api.openrouteservice.org https://cekgetir.com https://*.cekgetir.com",
+    "font-src 'self' https://*.gstatic.com https://fonts.googleapis.com https://cdnjs.cloudflare.com https://cekgetir.com https://*.cekgetir.com",
+    "connect-src 'self' https://cekgetir.com https://*.cekgetir.com https://api.cekgetir.com https://cekgetir.up.railway.app https://*.googleapis.com https://maps.googleapis.com https://maps.gstatic.com https://nominatim.openstreetmap.org https://api.openrouteservice.org https://api.opencagedata.com https://*.opencagedata.com https://photon.komoot.io",
+    "frame-src 'self' https://*.googleapis.com https://www.google.com/maps/ https://maps.google.com/ https://www.google.com/ https://*.gstatic.com https://cekgetir.com https://*.cekgetir.com",
     "worker-src 'self' blob:"
   ]
 
